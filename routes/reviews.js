d7f33a33c48c51b46b6dfff7fee5828346edf93f
Richard Knott <richardknott1970@gmail.com>
2021-12-21 07:15:49 -0800

    first deploy

---
+++ routes/reviews.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
+const Campground = require("../models/campground");
+const reviews = require("../controllers/reviews");
+const Review = require("../models/review");
+const ExpressError = require("../utils/ExpressError");
+const catchAsync = require("../utils/catchAsync");
+// const { reviewSchema } = require("../schemas.js");
+
+// const validateReview = (req, res, next) => {
+//   const { error } = reviewSchema.validate(req.body);
+//   if (error) {
+//     const msg = error.details.map((el) => el.message).join(",");
+//     throw new ExpressError(msg, 400);
+//   } else {
+//     next();
+//   }
+// };
+
+router.post("/", isLoggedIn, validateReview, catchAsync(reviews.createReview));
+
+router.delete(
+  "/:reviewId",
+  isLoggedIn,
+  isReviewAuthor,
+  catchAsync(reviews.deleteReview)
+);
+
+module.exports = router;
