bf5a40120e70e04f7ef09049eacccc22560cd514
rrichardknott <r.richardknott@gmail.com>
2021-12-21 19:53:24 -0800

    Update app.js

--- app.js
+++ app.js
@@ -154,6 +154,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
+// Comment
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Serving on Port ${port}`);
