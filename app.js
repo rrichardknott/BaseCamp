2def98cfc7e864a7135a19669d95f8fe1482c8db
rrichardknott <67433117+rrichardknott@users.noreply.github.com>
2022-12-07 23:33:08 -0800

    Update app.js

--- app.js
+++ app.js
@@ -86,6 +86,7 @@ const scriptSrcUrls = [
   "https://kit.fontawesome.com/",
   "https://cdnjs.cloudflare.com/",
   "https://cdn.jsdelivr.net",
+   "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
 ];
 const styleSrcUrls = [
   "https://kit-free.fontawesome.com/",
